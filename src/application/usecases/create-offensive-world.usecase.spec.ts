47b31c4d5042a5e7890b8ef7014b9352afe8dd48
Ruben Aguilera <radhcode@gmail.com>
2021-07-16 00:10:05 +0200

    test: using ts-mockito to mock domain interfaces

--- src/application/usecases/create-offensive-world.usecase.spec.ts
+++ src/application/usecases/create-offensive-world.usecase.spec.ts
@@ -1,13 +1,18 @@
 import { OffensiveWordService } from '../../domain/services/offensive-word.service';
 import {CreateOffensiveWordUseCase} from './create-offensive-word.usecase';
 import { OffensiveWordRepositoryMemory } from './../../infrastructure/repositories/offensive-word.repository.memory';
+import { mock, instance, when, verify } from 'ts-mockito';
+import { OffensiveWordRepository } from '../../domain/repositories/offensive-word.repository';
 
 describe('Create offensive word Use Case', () => {
 
     it('should create offensive word and persist', () => {
 
-        const useCase: CreateOffensiveWordUseCase = new CreateOffensiveWordUseCase(new OffensiveWordService(new OffensiveWordRepositoryMemory()));
+        const mockRepository = mock<OffensiveWordRepository>();
+        const spyRepository = jest.spyOn(mockRepository, 'save');
+        const useCase: CreateOffensiveWordUseCase = new CreateOffensiveWordUseCase(new OffensiveWordService(mockRepository));
         useCase.execute();
+        expect(spyRepository).toBeCalled();
 
     });
 
